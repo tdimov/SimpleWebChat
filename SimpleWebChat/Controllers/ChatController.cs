5d3a922be48ba712338c7ce07c9f3ae1af4caba1
tdimov <tihomirdimov92@gmail.com>
2013-10-09 21:47:45 +0300

    first upload

---
+++ SimpleWebChat/Controllers/ChatController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleWebChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        public ActionResult WebChat()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ SimpleWebChat/Hubs/Chat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SimpleWebChat.Hubs
+{
+    public class Chat : Hub
+    {
+        public void SendMessage(string message)
+        {
+            string username = Environment.MachineName.Substring(0, Environment.MachineName.Length - 3); //cut -PC from computer name
+            string msg = string.Format("{0}: {1}", username, message.Replace("<", " ").Replace(">", " "));
+            Clients.All.addMessage(msg);
+        }
+    }
+}
